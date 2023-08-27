ea60d2b1b0a7986f1f7eea4a5e45540f395de73c
skull162 <62290061+skull162@users.noreply.github.com>
2023-08-28 00:06:39 +0700

    Membuat Main Menu

---
+++ Assets/Script/SceneController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+   public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
