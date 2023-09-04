735fc722badbe0cc2b0aedd0145793adc6d0a5d8
tutdesiwa98 <tutdesiwasesanapande@gmail.com>
2023-09-04 16:49:37 +0700

    firstperson
    firstperson

---
+++ Assets/Player.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float mouseSensitivity = 3f;
+    [SerializeField] float movementSpeed = 5f;
+    [SerializeField] float mass = 1f;
+    [SerializeField] Transform cameraTransform;
+
+    CharacterController controller;
+    Vector3 velocity;
+    Vector2 look;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        UpdateGravity();
+        updateMovement();
+        updateLook();
+    }
+
+    void UpdateGravity()
+    {
+        var gravity = Physics.gravity * mass * Time.deltaTime;
+        velocity.y = controller.isGrounded ? -1f : velocity.y + gravity.y;
+    }
+
+    void updateMovement()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+
+        var input = new Vector3();
+        input += transform.forward * y;
+        input += transform.right * x;
+        input = Vector3.ClampMagnitude(input, 1f);
+
+        //transform.Translate(input * movementSpeed * Time.deltaTime, Space.World);
+        controller.Move((input * movementSpeed + velocity) * Time.deltaTime);
+    }
+    // Update is called once per frame
+    void updateLook()
+    {
+        look.x += Input.GetAxis("Mouse X") * mouseSensitivity;
+        look.y += Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        look.y = Mathf.Clamp(look.y, -89f, 89f);
+
+        cameraTransform.localRotation = Quaternion.Euler(-look.y, 0, 0);
+        transform.localRotation = Quaternion.Euler(0, look.x, 0);
+    }
+}
